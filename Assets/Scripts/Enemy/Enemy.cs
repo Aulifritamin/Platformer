49d9de7f1b8ea2278c4a4d20a1243a09d819df9b
Aleksandr Zhivov <aulifritamin@gmail.com>
2025-11-19 01:57:44 -0800

    refactored

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -3,15 +3,9 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D), typeof(Rigidbody2D))]
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private Transform _groundCheck;
-    [SerializeField] private LayerMask _groundLayer;
-    [SerializeField] private float _moveSpeed = 2f;
-    [SerializeField] private float _groundCheckDistance = 0.1f;
-    [SerializeField] private float _patrolDistance = 5f;
+    [SerializeField] private EnemyPatrol _patrol;
 
-    private Vector2 _startPosition;
     private Rigidbody2D _rigidbody;
-    private int _direction = 1;
 
     private void Awake()
     {
@@ -20,64 +14,19 @@ public class Enemy : MonoBehaviour
 
     private void Start()
     {
-        _startPosition = transform.position;
+        _patrol.Initialize(_rigidbody, transform);
     }
 
     private void FixedUpdate()
     {
-        Patrol();
-    }
-
-    private void Patrol()
-    {
-        float currentX = transform.position.x;
-        _rigidbody.velocity = new Vector2(_direction * _moveSpeed, _rigidbody.velocity.y);
-
-        if (!IsGroundAhead())
-        {
-            FlipDirection();
-            return;
-        }
-
-        if (_direction > 0 && currentX >= _startPosition.x + _patrolDistance)
-        {
-            FlipDirection();
-            return;
-        }
-
-        if (_direction < 0 && currentX <= _startPosition.x - _patrolDistance)
-        {
-            FlipDirection();
-            return;
-        }
-    }
-
-    private bool IsGroundAhead()
-    {
-        return Physics2D.Raycast(
-            _groundCheck.position,
-            Vector2.down,
-            _groundCheckDistance,
-            _groundLayer
-        );
-    }
-
-    private void FlipDirection()
-    {
-        _direction *= -1;
-
-        Vector3 scale = transform.localScale;
-        scale.x = -scale.x;
-        transform.localScale = scale;
+        _patrol.Patrol();
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-
-        Vector3 start = _groundCheck.position;
-        Vector3 end = _groundCheck.position + Vector3.down * _groundCheckDistance;
-
-        Gizmos.DrawLine(start, end);
+        if (_patrol != null)
+        {
+            _patrol.DrawGizmos();
+        }
     }
 }
